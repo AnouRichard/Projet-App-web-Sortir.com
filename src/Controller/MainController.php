c1c699f94572573ecc42678e6b2fa4c916d5fd9c
AnouRichard <59556069+AnouRichard@users.noreply.github.com>
2020-11-03 09:54:00 +0100

    Controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends Controller
+class MainController extends AbstractController
 {
     /**
      * @Route("/", name="home")
@@ -12,13 +12,5 @@ class MainController extends Controller
     public function home()
     {
         return $this->render("main/home.html.twig");
-    }
-
-    /**
-     * @Route("/about-us", name="about_us")
-     */
-    public function aboutUs()
-    {
-        return $this->render("main/about_us.html.twig");
     }
 }

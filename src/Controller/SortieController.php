8525e2770683d4641d838597c8d32d303ecfd253
valentin <voisinvalentin4@gmail.com>
2020-11-03 10:20:36 +0100

    SortieController

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,7 @@
 namespace App\Controller;
 
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;

a56a6929b741cbecfaed228bb3ea47af4855857a
valentin <voisinvalentin4@gmail.com>
2020-11-05 11:21:48 +0100

    sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 
 class SortieType extends AbstractType
 {

aba6f1152d082b410698ce27ee7a3325d611b9fd
valentin <voisinvalentin4@gmail.com>
2020-11-06 15:01:00 +0100

    sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -43,7 +43,7 @@ class SortieController extends AbstractController
      */
     public function ajaxAction(Request $request,EntityManagerInterface $em) {
         $repo = $em->getRepository(Ville::class);
-        $idVille=$request->request->get('idville');
+        $idVille=$request->data->get('idville');
         $ville=$repo->find($idVille);
         $repo = $em->getRepository(Lieux::class);
         $lieux=$repo->findBy(array("ville" => $ville));

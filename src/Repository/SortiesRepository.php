aa0cf3ae2f7874e24b7b6930bd751e7457fd8ae9
valentin <voisinvalentin4@gmail.com>
2020-11-11 22:04:44 +0100

    SortieController

--- src/Repository/SortiesRepository.php
+++ src/Repository/SortiesRepository.php
@@ -30,11 +30,11 @@ class SortiesRepository extends ServiceEntityRepository
         foreach ($parametres as $key=> $value){
             dump($key);
             switch ($key) {
-                case "nomSortie":
+                case "nomSort":
                     $conditions=$conditions."s.nom like '%".$value."%'";
                     break;
                 case "campus":
-                    $conditions=$conditions."c = ".$value;
+                    $conditions=$conditions."EXISTS(SELECT t0.id AS id_1, t0.pseudo AS pseudo_2, t0.nom AS nom_3, t0.prenom AS prenom_4, t0.telephone AS telephone_5, t0.mail AS mail_6, t0.mot_de_passe AS mot_de_passe_7, t0.administrateur AS administrateur_8, t0.actif AS actif_9, t0.campus_id AS campus_id_10 FROM participants where t0.campus_id=1)";
                     break;
                 case "sortieOrga":
                     $conditions=$conditions.$verifor." s.organisateur = ".$user->getId();

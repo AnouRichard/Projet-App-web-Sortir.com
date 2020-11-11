f4a1b52dc66ababc9e71c13482db3c1de87b5565
valentin <voisinvalentin4@gmail.com>
2020-11-11 20:18:13 +0100

    SortieController

--- src/Repository/SortiesRepository.php
+++ src/Repository/SortiesRepository.php
@@ -47,7 +47,7 @@ class SortiesRepository extends ServiceEntityRepository
             ->innerJoin('p.campus','c')
 
             //->andWhere('s.etat = :val')
-            ->andWhere('p = 4,s=1')
+            ->andWhere('p = 4 and s=1')
 
             // ->setParameter('val',1)
             //->setParameter('val2',4)

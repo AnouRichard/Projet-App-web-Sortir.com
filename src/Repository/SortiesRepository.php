4f7996cbbd8e5aabef136c20b15a03969662a9c4
valentin <voisinvalentin4@gmail.com>
2020-11-11 21:03:35 +0100

    SortieController

--- src/Repository/SortiesRepository.php
+++ src/Repository/SortiesRepository.php
@@ -25,7 +25,7 @@ class SortiesRepository extends ServiceEntityRepository
 
     public function findSorties($parametres,$user)
     {
-        $verifor="";
+        $verifor="and";
         $conditions="";
         foreach ($parametres as $key=> $value){
             dump($key);
